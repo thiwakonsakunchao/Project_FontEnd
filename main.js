b3ef6f71103f9fef1bbc4f39ee9ccbab5af68dd2
Fame <fame5454@gmail.com>
2023-02-26 12:53:30 +0700

    Add Menu

--- main.js
+++ main.js
@@ -1,56 +1,21 @@
 // Category
-jQuery(window).on('load', function () {
-    $('body').removeClass('body-fixed');
-
-    //activating tab of filter
-    let targets = document.querySelectorAll(".filter");
-    let activeTab = 0;
-    let old = 0;
-    let dur = 0.4;
-    let animation;
-
-    for (let i = 0; i < targets.length; i++) {
-        targets[i].index = i;
-        targets[i].addEventListener("click", moveBar);
-    }
-
-    // initial position on first === All 
-    gsap.set(".filter-active", {
-        x: targets[0].offsetLeft,
-        width: targets[0].offsetWidth
+$(document).ready(function () {
+    var $list = $(".card-product-box .card").hide(),
+      $curr;
+  
+    $(".button")
+      .on("click", function () {
+        var $this = $(this);
+        $this.addClass("active").siblings().removeClass("active");
+        $curr = $list.filter("." + this.id).hide();
+        $curr.slice(0, 10).show(400);
+        $list.not($curr).hide(300);
+      })
+      .filter(".active")
+      .click();
+  
+    $("#LoadMore").on("click", function () {
+      $curr.filter(":hidden").slice(0, 4).slideDown("slow");
     });
-
-    function moveBar() {
-        if (this.index != activeTab) {
-            if (animation && animation.isActive()) {
-                animation.progress(1);
-            }
-            animation = gsap.timeline({
-                defaults: {
-                    duration: 0.4
-                }
-            });
-            old = activeTab;
-            activeTab = this.index;
-            animation.to(".filter-active", {
-                x: targets[activeTab].offsetLeft,
-                width: targets[activeTab].offsetWidth
-            });
-
-            animation.to(targets[old], {
-                color: "#0d0d25",
-                ease: "none"
-            }, 0);
-            animation.to(targets[activeTab], {
-                color: "#fff",
-                ease: "none"
-            }, 0);
-
-        }
-
-    }
-});
-
-
-
-
+  });
+  
